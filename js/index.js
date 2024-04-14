48bcb2e2f32685e76876c7e44b1dec59b6b6ac87
Ilya <you@example.com>
2024-04-14 23:27:02 +0300

    second commit

--- js/index.js
+++ js/index.js
@@ -36,11 +36,15 @@ window.addEventListener("DOMContentLoaded", () => {
 
     // timer
 
-    const deadLine = "2024-04-07"
+    const deadLine = "2024-04-08"
 
     const getTimeRemaining = (endTime) =>{
-        const t = Date.parse(endTime) - Date.parse(new Date()),
-        days = Math.floor(t / (1000 * 60 * 60 * 24)),
+        const t = Date.parse(endTime) - Date.parse(new Date())
+        if (t <= 0){
+            return null
+        }
+
+        const days = Math.floor(t / (1000 * 60 * 60 * 24)),
         hours = Math.floor((t / (1000 * 60 * 60) & 24)),
         minutes = Math.floor((t / 1000 / 60) % 60),
         seconds = Math.floor((t / 1000) % 60);
@@ -74,17 +78,87 @@ window.addEventListener("DOMContentLoaded", () => {
 
         function updateClock() {
             const objTime = getTimeRemaining(endTime);
+            if (objTime != null){
+                days.innerHTML = getZero(objTime["days"])
+                hours.innerHTML = getZero(objTime["hours"])
+                minutes.innerHTML = getZero(objTime["minutes"])
+                seconds.innerHTML = getZero(objTime["seconds"])
 
-            days.innerHTML = getZero(objTime["days"])
-            hours.innerHTML = getZero(objTime["hours"])
-            minutes.innerHTML = getZero(objTime["minutes"])
-            seconds.innerHTML = getZero(objTime["seconds"])
+                if (objTime["t"] <= 0){
+                    clearInterval(timeInterval)
+                }
+        } else {
+            console.log("Дата прошла")
+            clearInterval(timeInterval)
 
-            if (objTime["t"] <= 0){
-                clearInterval(timeInterval)
-            }
+            const promotion__timer = document.querySelector(".promotion")
+            promotion__timer.textContent = ""
+        }
         }
     }
 
     setClock(deadLine)
+
+    // классы для карточек
+    
+    class MenuCard{
+        constructor(src, alt, title, description, price){
+            this.src = src
+            this.alt = alt
+            this.title = title
+            this.description = description
+            this.price = price
+            this.transfer = 27;
+            this.changeUSDToUAH()
+        }
+
+        changeUSDToUAH(){
+            this.price = this.price * this.transfer  
+        }
+
+        render(){
+            const element = document.createElement("div")
+            element.innerHTML = `
+                <div class="menu__item">
+                <img src="${this.src}" alt="${this.alt}">
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.description}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                </div>
+            </div>
+        `
+
+        const container = document.querySelector(".menu__field .container")
+        container.append(element)
+        }
+    }
+
+    new MenuCard(
+        src="img/tabs/vegy.jpg",
+        alt="vegy",
+        title='Меню "Фитнес"',
+        description='Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        price=9
+    ).render();
+
+    new MenuCard(
+        "img/tabs/post.jpg",
+        "post",
+        'Меню "Постное"',
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+        14,
+        ".menu .container"
+    ).render();
+
+    new MenuCard(
+        "img/tabs/elite.jpg",
+        "elite",
+        'Меню “Премиум”',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        21,
+        ".menu .container"
+    ).render();
 });
